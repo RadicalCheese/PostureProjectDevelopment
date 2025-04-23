22939de9dec945987eb8a7dd388aa864900de2bc
RadicalCheese <124704933+RadicalCheese@users.noreply.github.com>
2025-04-23 20:15:46 +0100

    Update simultaneous_execution.py

--- simultaneous_execution.py
+++ simultaneous_execution.py
@@ -2,9 +2,12 @@ import threading
 import subprocess
 import time
 
+#running this script
 def run_script(script_name):
     subprocess.run(["python", script_name])
 
+#ensuring the code runs when executed directly, not imported as a module
+#uses threading to run different scripts
 if __name__ == "__main__":
     data_thread = threading.Thread(target=run_script, args=("acc_gyro.py",))
     cleaning_thread = threading.Thread(target=run_script, args=("cleaning.py",))
@@ -12,15 +15,18 @@ if __name__ == "__main__":
     flask_api_thread = threading.Thread(target=run_script, args=("sensor_api.py",))
     daily_api_thread = threading.Thread(target=run_script, args=("daily_api.py",))
     
-
+    #printing to console that the scripts are all currently running
     print("Scripts are currently executing.")
 
+    #starts running each thread in parallel
+    #execution begins almost simultaneously
     data_thread.start()
     cleaning_thread.start()
     averages_thread.start()
     flask_api_thread.start()
     daily_api_thread.start()
-    
+
+    #all threads must be finished executing before the code stops executing
     data_thread.join()
     cleaning_thread.join()
     averages_thread.join()
