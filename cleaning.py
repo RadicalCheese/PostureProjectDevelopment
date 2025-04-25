91efa2f550938b721d10a219a53e07964cfbab0f
RadicalCheese <124704933+RadicalCheese@users.noreply.github.com>
2025-04-25 23:03:52 +0100

    SenseHAT Code

--- cleaning.py
+++ cleaning.py
@@ -4,7 +4,7 @@ from datetime import datetime, date
 from matplotlib.animation import FuncAnimation
 import os
 
-file_path = '/home/isabelconaghan/Documents/MPU6050/get_mpu6050_data.csv'
+file_path = '/home/isabelconaghan/Documents/SenseHAT/senhat.csv'
 refresh_interval = 2000 
 
 #establishing scatter plot
@@ -14,7 +14,7 @@ figure, axis = plot.subplots()
 line, = axis.plot([], [], 'o-', label='Tilt Angle')
 axis.set_xlabel("Time")
 axis.set_ylabel("Angle in Degrees")
-axis.set_title("MPU6050 Live Angles")
+axis.set_title("SenseHAT Live Angles")
 plot.xticks(rotation=45)
 plot.tight_layout()
 axis.legend()
@@ -35,32 +35,44 @@ def update(frame):
     if not os.path.exists(file_path):
         return
 
+    #reads in the csv file
     df = pd.read_csv(file_path)
+    #removes whitespaces
     df.columns = df.columns.str.strip()
 
-    # Rename if needed
-    if 'angle' in df.columns and 'overall_angle' not in df.columns:
-        df.rename(columns={'angle': 'overall_angle'}, inplace=True)
-    if 'timestamp' in df.columns and 'time' not in df.columns:
-        df.rename(columns={'timestamp': 'time'}, inplace=True)
+    #renaming columns if needed
+    if 'angle' in df.columns and 'angle' not in df.columns:
+        df.rename(columns={'angle': 'angle'}, inplace=True)
+    if 'time' in df.columns and 'time' not in df.columns:
+        df.rename(columns={'time': 'time'}, inplace=True)
 
+    #cleans the data by making sure the data is in the right formats
+    #if time or angle data can't be parsed, return as NaN or NaT
     df['time'] = pd.to_datetime(df['time'], errors='coerce')
-    df['overall_angle'] = pd.to_numeric(df['overall_angle'], errors='coerce')
-    df.dropna(subset=['time', 'overall_angle'], inplace=True)
+    df['angle'] = pd.to_numeric(df['angle'], errors='coerce')
+    #drops null values
+    df.dropna(subset=['time', 'angle'], inplace=True)
+    #rounds values to one decimal place
     df = df.round(1)
+    #only graph last 10 entries to prevent clutter
     df = df.tail(10)
+    
+    #writing to a new csv file
+    cleaned_file_path = '/home/isabelconaghan/Documents/SenseHAT/cleaned_sensehat_data.csv'
+    df.to_csv(cleaned_file_path, index=False)
 
+    #plotting the updated data
     axis.clear()
-    axis.plot(df['time'], df['overall_angle'], 'o-', label='Tilt Angle')
+    axis.plot(df['time'], df['angle'], 'o-', label='Tilt Angle')
     axis.set_xlabel("Time")
     axis.set_ylabel("Angle in Degrees")
-    axis.set_title("MPU6050 Live Angles")
+    axis.set_title("SenseHAT Live Angles")
     axis.legend()
     plot.xticks(rotation=45)
     plot.tight_layout()
     
     
 
-#runs animation
+#runs animation to update live data
 animation = FuncAnimation(figure, update, interval=refresh_interval, cache_frame_data=False)
 plot.show()
